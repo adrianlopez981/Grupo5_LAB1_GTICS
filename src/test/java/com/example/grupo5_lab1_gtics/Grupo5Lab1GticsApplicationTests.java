874389e69a7298e9752f9ad1fffa866ad85219ad
Adrian Lopez <adrian.lopez@pucp.edu.pe>
2023-03-28 21:14:15 -0500

    first commit

---
+++ src/test/java/com/example/grupo5_lab1_gtics/Grupo5Lab1GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo5_lab1_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo5Lab1GticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
