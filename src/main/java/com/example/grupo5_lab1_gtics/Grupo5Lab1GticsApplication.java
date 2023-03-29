58badad85c3e77fb47d71e307b73daa4faf466e0
Adrian Lopez <adrian.lopez@pucp.edu.pe>
2023-03-28 21:14:15 -0500

    first commit

---
+++ src/main/java/com/example/grupo5_lab1_gtics/Grupo5Lab1GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupo5_lab1_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo5Lab1GticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo5Lab1GticsApplication.class, args);
+	}
+
+}
